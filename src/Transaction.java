e72dec15d44afa8b280e1430043e9a7784f73c97
adrian2189 <adrianfadilah2189@gmail.com>
2024-11-24 09:05:09 +0800

    Class Transaction dan TransactionManger

--- src/Transaction.java
+++ src/Transaction.java
@@ -8,5 +8,43 @@
  * @author User
  */
 public class Transaction {
-    
+ private String date;
+    private String description;
+    private double amount;
+
+    public Transaction(String date, String description, double amount) {
+        this.date = date;
+        this.description = description;
+        this.amount = amount;
+    }
+
+    // Encapsulation: Getter dan Setter
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return date + " - " + description + " : " + amount;
+    }   
 }
--- src/TransactionManager.java
+++ src/TransactionManager.java
@@ -7,6 +7,29 @@
  *
  * @author User
  */
+
+import java.util.ArrayList;
+
 public class TransactionManager {
-    
+ private ArrayList<Transaction> transactions;
+
+    public TransactionManager() {
+        transactions = new ArrayList<>();
+    }
+
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+    }
+
+    public void updateTransaction(int index, Transaction transaction) {
+        transactions.set(index, transaction);
+    }
+
+    public void deleteTransaction(int index) {
+        transactions.remove(index);
+    }
+
+    public ArrayList<Transaction> getTransactions() {
+        return transactions;
+    }  
 }
